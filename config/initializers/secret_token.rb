c8516b7398b3ecaadb9342d74d5842f8d4ca6b08
Michael Stricklen <michael.stricklen@gmail.com>
2013-02-26 20:01:58 -0500

    Rails 3 Generated App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scrumpoker::Application.config.secret_token = 'fe2a227c788d07841263e59510347c757c8b481fa1950154bdff18c9471bd2ddcc9641a331660412d18319b665405ef70d198f4122cfce41800b2041cedbe164'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Scrumpoker::Application.config.session_store :cookie_store, key: '_scrumpoker_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Scrumpoker::Application.config.session_store :active_record_store
